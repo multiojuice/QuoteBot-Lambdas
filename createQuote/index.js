5c712164f4cb7e196e965ec1f259c9e806eb28ba
Owen Sullivan <multiojuice@gmail.com>
2020-05-13 19:33:51 -0400

    Changed to acios

--- createQuote/index.js
+++ createQuote/index.js
@@ -1,18 +1,18 @@
 const AWS = require('aws-sdk');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 exports.handler = async (event) => {
     console.log('BOOM', event.body);
     
-    const  response = await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_KEY}/sendMessage`, {
-      method: 'POST',
-      body: JSON.stringify({
-        chat_id: event.body.chat.id,
-        text: 'This is working'
-      })
-    });
-    
-    console.log('CHAOS', event.body);
+    const response = await axios.post(
+      `https://api.telegram.org/bot${process.env.TELEGRAM_KEY}/sendMessage`,
+      {
+        chat_id: JSON.parse(event.body).message.chat.id,
+        text: "Polo!!"
+      }
+    );
+  
+    console.log('CHAOS', response);
     
     return response;
 };
